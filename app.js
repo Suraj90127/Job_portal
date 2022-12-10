09e7f68d0e0ed20ed69e73ed589f58a5e6d14ec8
rjsurya <surajsingh26574@gmail.com>
2022-12-10 10:04:45 -0800

    rj commit

---
+++ app.js
@@ -0,0 +1,77 @@
+//change navbar cilor 
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll>150){
+            $(".navbar").css("background", "#222");
+            $(".navbar").css("box-shodow", "rgba(0, 0, 0, 0.1) 0px 4px 12px");
+                }
+        else{
+            $(".navbar").css("background", "transparent");
+            $(".navbar").css("box-shodow", "none");  
+        }
+    })
+});
+
+//smooth scroll
+
+var navbarHeight = $(".navbar").outerHeight();
+$(".navbar-menu a").click(function(e){
+    var targetHref = $(this).attr("href");
+    $("html,body").animate({
+        screenTop: $(targetHref).top - navbarHeight
+    },1000)
+    e.PreventDefault();
+})
+
+//navbar mobile version
+
+const mobile = document.querySelector(".menu-toggle");
+const mobileLink = document.querySelector(".navbar-menu");
+
+mobile.addEventListener("click",function(){
+    mobile.classList.toggle("is-active");
+    mobileLink.classList.toggle("active")
+})
+
+mobileLink.addEventListener("click", function(){
+    const menuBars = document.querySelector(".is-active");
+    if(window.innerWidth <= 768 && menuBars){
+        mobile.classList.toggle("is-active");
+    mobileLink.classList.remove("active")
+    }
+})
+
+
+//swiper
+
+var swiper = new Swiper(".mySwiper",
+{
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    slidesPerView:1,
+    spaceBetween:10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768:{
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024:{
+            slidesPerView: 3,
+            spaceBetween:50,
+        }
+    }
+
+})
